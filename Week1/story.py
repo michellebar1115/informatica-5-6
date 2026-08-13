2e14db7715f1a84336401075badc9a4be5d12fe8
michellebar1115 <michellebar1115@gmail.com>
2026-08-13 15:35:18 +0000

    story.py

--- Week1/story.py
+++ Week1/story.py
@@ -15,15 +15,19 @@ def main():
     # print(planet)
 
 
-    name = input("What is your name? ")
-    color = input("Tell me a color: ")
-    adj = input("Name a random adjective: ")
-    goal = input("Goal you would like to achive: ")
+    name = input("What is your name? ").strip().title()
+    color = input("Tell me a color: ").strip().lower()
+    adj = input("Name a random adjective: ").strip().lower()
+    goal = input("Goal you would like to achive: ").strip().lower()
 
     print(f"Hello, {name}!")
+    print()
+
     print("This is your story:")
     print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}")
-
+    print()
+    print("This is your story in uppercase:")
+    print(f"At dawn the sky turned {color}, and the air felt {adj}. I decided today I will finally {goal}".upper())
 
 if __name__ == "__main__":
     main()
