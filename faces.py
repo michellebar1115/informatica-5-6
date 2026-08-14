cc94e7c145df710c332abb74e9d64ab4a27ea6d0
michellebar1115 <michellebar1115@gmail.com>
2026-08-14 15:31:11 +0000

    faces.py

---
+++ faces.py
@@ -0,0 +1,7 @@
+def main():
+
+    text = input("Write a message and a keyboard smily face or frowny face:")
+    print(text.replace(":)","🙂").replace(":(","🙁"))
+if __name__ == "__main__":
+    main()
+
