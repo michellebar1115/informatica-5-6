1c7100c519459d72a50ddc0cfc34904a431f0dbf
michellebar1115 <michellebar1115@gmail.com>
2026-08-18 19:44:30 +0000

    rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -8,14 +8,14 @@ def main():
     print("O"*w)
     print("O"*w)
 
-    perm = 3+3+w+w
+    perm = 5+5+w+w
     area = 5*w
     print("Perimeter:",perm)
     print("Area:",area)
 
-    #d = 5**2 + w**2
-    #d2= (d)/.5
-    #print(d2)
+    d = 5**2 + w**2
+    d2= d/.5
+    print("Diagonal:",d2)
 
 if __name__ == "__main__":
     main()
