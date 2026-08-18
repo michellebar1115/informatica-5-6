7d28a9dcf554797337c29eb9914106dcbf335951
michellebar1115 <michellebar1115@gmail.com>
2026-08-18 20:15:37 +0000

    currency.py

--- currency.py
+++ currency.py
@@ -1,12 +1,24 @@
 def main():
 
-    pesos=imput("How many pesos do you have? ")
-    soles=imput("How many soles do you have? ")
-    reais=imput("How many reais do you have? ")
+    pesosc=float(input("How many pesos do you have? "))
+    soles=float(input("How many soles do you have? "))
+    reais=float(input("How many reais do you have? "))
 
-    pesost=
-    solest=
-    reaist=
+    pesosctu= pesosc*0.00032
+    solestu= soles*0.3
+    reaistu= reais*0.19
+
+    total=pesosctu+solestu+reaistu
+    total=round(total,2)
+    print("Total in USD:",total)
+
+    pesosctp= pesosc*0.0054
+    solestp= soles*5.07
+    reaistp= reais*3.27
+
+    total2=pesosctp+solestp+reaistp
+    total2=round(total2,2)
+    print("Total in pesos:",total2)
 
 
 if __name__ == "__main__":
