91256568fbf33de48ef6b82faa3b326fa8fdc068
michellebar1115 <michellebar1115@gmail.com>
2026-08-18 19:53:58 +0000

    rectangle.py

--- currency.py
+++ currency.py
@@ -3,5 +3,11 @@ def main():
     pesos=imput("How many pesos do you have? ")
     soles=imput("How many soles do you have? ")
     reais=imput("How many reais do you have? ")
+
+    pesost=
+    solest=
+    reaist=
+
+
 if __name__ == "__main__":
     main()
--- rectangle.py
+++ rectangle.py
@@ -13,7 +13,7 @@ def main():
     print("Perimeter:",perm)
     print("Area:",area)
 
-    d = (5**2 + w**2)
+    d = (5**2 + w**2)**.5
     print("Diagonal:",d)
 
 if __name__ == "__main__":
