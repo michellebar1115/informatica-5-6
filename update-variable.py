1f682341ab8dc5f7d3d9b65d0326537a31072eb0
michellebar1115 <michellebar1115@gmail.com>
2026-08-19 18:00:03 +0000

    update-variable.py

---
+++ update-variable.py
@@ -0,0 +1,36 @@
+def main():
+    # Augmented Assignment Operator
+
+    score = 10
+    # score2 = score + 10. This and the one below is the same just simpler.
+    score += 10
+    print(score)
+
+    # Substraction Assignment Operator
+
+    grade = 100
+    grade -= 15
+    print(grade)
+
+    # Multiplication Assignment Operator
+
+    balance = 10
+    balance *= 4
+    print(balance)
+
+    # Division Assignment Operator
+
+    x = 10
+    x /=5
+    print(x)
+
+    # Modulus Assignment Operator
+    
+    a = 10
+    b = 4
+
+    a %= b
+    print(a)
+
+if __name__=="__main__":
+    main()
