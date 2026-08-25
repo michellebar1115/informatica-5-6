37a2a3cae0e5a3ef7898e913b212149568504dd0
michellebar1115 <michellebar1115@gmail.com>
2026-08-25 20:02:51 +0000

    Elif

---
+++ rating.py
@@ -0,0 +1,20 @@
+def main():
+
+    print("Michy's Pastaria")
+
+    rating = float(input("What do you rate Michy's Pastaria from 0-5? Use decimals. : "))
+
+    if rating > 4.5:
+        print("Perfection")
+    elif rating > 4:
+        print("Excellent")
+    elif rating > 3:
+        print("Good")
+    elif rating > 2:
+        print("Fair")
+    else:
+        print("Poor")
+
+if __name__ == "__main__":
+    main()
+
