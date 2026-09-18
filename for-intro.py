0ffd83c02308610543c3e605d9349ffbd0152e9c
michellebar1115 <michellebar1115@gmail.com>
2026-09-18 20:19:07 +0000

    idek

---
+++ for-intro.py
@@ -0,0 +1,10 @@
+def main():
+
+    doctrine = ["Faith in Jesus Christ","Repentance","Baptism","Recieving the gift of the Holy Ghost", "Endure to the end"]
+    for _ in range(4):
+        break
+    for i in doctrine:
+        print(_+1,i)
+
+if __name__ == "__main__":
+    main()
