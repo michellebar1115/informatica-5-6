6fa1f3a5138390a5aa4e75a2b29bdb476df57ef2
michellebar1115 <michellebar1115@gmail.com>
2026-09-18 21:22:35 +0000

    all done

--- Week5/for-intro.py
+++ Week5/for-intro.py
@@ -1,10 +1,9 @@
 def main():
 
     doctrine = ["Faith in Jesus Christ","Repentance","Baptism","Recieving the gift of the Holy Ghost", "Endure to the end"]
-    for _ in range(4):
-        break
-    for i in doctrine:
-        print(_+1,i)
+
+    for i in range(len(doctrine)):
+        print(f"{i+1} {doctrine[i]}")
 
 if __name__ == "__main__":
     main()
